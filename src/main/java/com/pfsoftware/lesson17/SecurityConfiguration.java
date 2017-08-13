d2df0dc6a4b501680d1c1f8ad0726d15e3a15841
dave wolf <dave45678@gmail.com>
2017-08-13 16:04:28 -0400

    initial commit

---
+++ src/main/java/com/pfsoftware/lesson17/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.pfsoftware.lesson17;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration 
+			extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) 
+								throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("dave").password("password").roles("USER")
+			.and()
+			.withUser("Bart").password("password").roles("USER");
+
+	}
+
+}
